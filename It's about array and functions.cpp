596c20ef89e927ab00cd3ca7d7dec2ccf9b0a6af
Mert Umul <84383448+Mertumul@users.noreply.github.com>
2022-01-17 21:52:19 +0300

    hOMEWORK
    IT'S MY HOMEWORK

---
+++ It's about array and functions.cpp
@@ -0,0 +1,227 @@
+﻿/****************************************************************************
+**					SAKARYA ÜNİVERSİTESİ
+**			         BİLGİSAYAR VE BİLİŞİM BİLİMLERİ FAKÜLTESİ
+**				    BİLGİSAYAR MÜHENDİSLİĞİ BÖLÜMÜ
+**				          PROGRAMLAMAYA GİRİŞİ DERSİ
+**
+**				ÖDEV NUMARASI:1
+**				ÖĞRENCİ ADI:MERT UMUL
+**				ÖĞRENCİ NUMARASI.:B201210049
+**				DERS GRUBU:C
+****************************************************************************/
+#include <iostream>
+using namespace std;
+#include <locale.h>
+#include<string>
+
+//oluşturulan il stringi dizinin içindemi diye sorgulayan fonksiyon yazdım.Eğer içindeyse true değise false veriyor.
+bool arama(string türkiye_iilleri[], int len, string seek)
+{
+	for (int i = 0; i < len; i++)
+	{
+		if (türkiye_iilleri[i] == seek)
+			return true;
+	}
+
+	return false;
+}
+//burada bu fonksiyonu oluşturmamım genel amacı bir a sayısı verdiğimde o a sayısına eşdeğer karakterli ili rastgele oluşturmak : Örnğin 3 verirsem 3 karakterli il oluşturur ve kontrol eder eğer bulunamazsa bulunamadı bulunursa bulundu ve il adını yazdırır genel mantığı budur.
+void il_oluşrma(int a, string türkiye_iileri[]) {
+	srand(time(NULL));
+	int srastgelesayilar[14];//burada bir dizi oluşturdum değerlerini sonradan atadım 14 yazmamım sebebi en büyük il Afyonkarahisar olduğundan en fazla bu kadar yer kaplayacağı için
+	string oluşturulanil;
+	bool bulundu = false;//bunu oluşturmamın genel amacı programı sonlandırmak ve il bulunamaz ise bulunamadı yazdırmak.
+	//100000 kere deneme yapar.
+	for (int j = 0; j < 100000; j++) {
+
+		//dizinin içindeki elemanlara rastgele sayı tanımlar.
+		for (int i = 0; i < a; i++) {
+			srastgelesayilar[i] = rand() % 80;// 80 yazmamın sebebi indisle 0 dan başlayıp 80 kadar gitmesi.
+		}
+		
+		int ekrastgele_sayı = türkiye_iileri[srastgelesayilar[0]].size();
+		//bir ekrastgele_sayı tanmladım bunun genel amacı rastgele illerden en küçük indisli elemanı bulmak ve ona göre döngüyü kontrol etmektir.Bu döngüde en küçük indisli ilin indis sayısını bulmak.
+		for (int i = 1; i < a; i++) {
+			if (ekrastgele_sayı > türkiye_iileri[srastgelesayilar[i]].size())
+				ekrastgele_sayı = türkiye_iileri[srastgelesayilar[i]].size();
+			else
+				continue;
+		}//yukarıda fonksiyona 3 sayısı girerse 3 haneli iller rastgele oluşturulur ve türkiye_iiller dizisinin içinde mi kontrol edilir içindeyse bulundu deyip hangi ilin olduğu yazdırılır.
+		if (a == 3) {
+			for (int k = 0; k < ekrastgele_sayı; k++) {//ekrastgele_sayı kullandım çünkü en küçük indisliye göre diğer elemanların indisine bakılacak.Öreneğin Muğla,Sakarya,Afyonkarahisar bunlardan en küçük indisli muğla ve 5 tir 5 ten sonra mupladan eleman gelmez bu yüzden bakmaya gerek yoktur.
+				oluşturulanil = türkiye_iileri[srastgelesayilar[0]].substr(k, 1) + türkiye_iileri[srastgelesayilar[1]].substr(k, 1) + türkiye_iileri[srastgelesayilar[2]].substr(k, 1);//il oluşturma
+				if (arama(türkiye_iileri, 81, oluşturulanil) && !bulundu) {//arama fonksiyonunu kullanır oluşturulan il türkiye_iller dizisinin içinde mi diye bakılır.
+					cout << " bulundu=" << "  " << oluşturulanil << endl;//bulunan ili yazdırır.
+					bulundu = true;//eğer bool true gelirse işlemi durdurur.
+				}
+			}
+		}//yukarıda fonksiyona 4 sayısı girerse 4 haneli iller rastgele oluşturulur ve türkiye_iiller dizisinin içinde mi kontrol edilir içindeyse bulundu deyip hangi ilin olduğu yazdırılır.
+		else if (a == 4) {
+			for (int k = 0; k < ekrastgele_sayı; k++) {
+
+				oluşturulanil = türkiye_iileri[srastgelesayilar[0]].substr(k, 1) + türkiye_iileri[srastgelesayilar[1]].substr(k, 1) + türkiye_iileri[srastgelesayilar[2]].substr(k, 1) + türkiye_iileri[srastgelesayilar[3]].substr(k, 1);
+				if (arama(türkiye_iileri, 81, oluşturulanil) && !bulundu) {
+					cout << " bulundu=" << "  " << oluşturulanil << endl;
+					bulundu = true;
+				}
+			}
+
+		}//yukarıda fonksiyona 5 sayısı girerse 5 haneli iller rastgele oluşturulur ve türkiye_iiller dizisinin içinde mi kontrol edilir içindeyse bulundu deyip hangi ilin olduğu yazdırılır.
+		else if (a == 5) {
+			for (int k = 0; k < ekrastgele_sayı; k++) {
+				oluşturulanil = türkiye_iileri[srastgelesayilar[0]].substr(k, 1) + türkiye_iileri[srastgelesayilar[1]].substr(k, 1) + türkiye_iileri[srastgelesayilar[2]].substr(k, 1) + türkiye_iileri[srastgelesayilar[3]].substr(k, 1) + türkiye_iileri[srastgelesayilar[4]].substr(k, 1);
+				if (arama(türkiye_iileri, 81, oluşturulanil) && !bulundu) {
+					cout << " bulundu=" << "  " << oluşturulanil << endl;
+					bulundu = true;
+				}
+			}
+		}//yukarıda fonksiyona 6 sayısı girerse 6 haneli iller rastgele oluşturulur ve türkiye_iiller dizisinin içinde mi kontrol edilir içindeyse bulundu deyip hangi ilin olduğu yazdırılır.
+		else if (a == 6) {
+			for (int k = 0; k < ekrastgele_sayı; k++) {
+				oluşturulanil = türkiye_iileri[srastgelesayilar[0]].substr(k, 1) + türkiye_iileri[srastgelesayilar[1]].substr(k, 1) + türkiye_iileri[srastgelesayilar[2]].substr(k, 1) + türkiye_iileri[srastgelesayilar[3]].substr(k, 1) + türkiye_iileri[srastgelesayilar[4]].substr(k, 1) + türkiye_iileri[srastgelesayilar[5]].substr(k, 1);
+				if (arama(türkiye_iileri, 81, oluşturulanil) && !bulundu) {
+					cout << " bulundu=" << "  " << oluşturulanil << endl;
+					bulundu = true;
+				}
+			}
+		}//yukarıda fonksiyona 7 sayısı girerse 7 haneli iller rastgele oluşturulur ve türkiye_iiller dizisinin içinde mi kontrol edilir içindeyse bulundu deyip hangi ilin olduğu yazdırılır.
+		else if (a == 7) {
+			for (int k = 0; k < ekrastgele_sayı; k++) {
+				oluşturulanil = türkiye_iileri[srastgelesayilar[0]].substr(k, 1) + türkiye_iileri[srastgelesayilar[1]].substr(k, 1) + türkiye_iileri[srastgelesayilar[2]].substr(k, 1) + türkiye_iileri[srastgelesayilar[3]].substr(k, 1) +
+					türkiye_iileri[srastgelesayilar[4]].substr(k, 1) + türkiye_iileri[srastgelesayilar[5]].substr(k, 1) + türkiye_iileri[srastgelesayilar[6]].substr(k, 1);
+				if (arama(türkiye_iileri, 81, oluşturulanil) && !bulundu) {
+					cout << " bulundu=" << "  " << oluşturulanil << endl;
+					bulundu = true;
+				}
+			}
+		}//yukarıda fonksiyona 8 sayısı girerse 8 haneli iller rastgele oluşturulur ve türkiye_iiller dizisinin içinde mi kontrol edilir içindeyse bulundu deyip hangi ilin olduğu yazdırılır.
+		else if (a == 8) {
+			for (int k = 0; k < ekrastgele_sayı; k++) {
+				oluşturulanil = türkiye_iileri[srastgelesayilar[0]].substr(k, 1) + türkiye_iileri[srastgelesayilar[1]].substr(k, 1) + türkiye_iileri[srastgelesayilar[2]].substr(k, 1) + türkiye_iileri[srastgelesayilar[3]].substr(k, 1) +
+					türkiye_iileri[srastgelesayilar[4]].substr(k, 1) + türkiye_iileri[srastgelesayilar[5]].substr(k, 1) + türkiye_iileri[srastgelesayilar[6]].substr(k, 1) + türkiye_iileri[srastgelesayilar[7]].substr(k, 1);
+				if (arama(türkiye_iileri, 81, oluşturulanil) && !bulundu) {
+					cout << " bulundu=" << "  " << oluşturulanil << endl;
+					bulundu = true;
+				}
+			}
+		}//yukarıda fonksiyona 9 sayısı girerse 9 haneli iller rastgele oluşturulur ve türkiye_iiller dizisinin içinde mi kontrol edilir içindeyse bulundu deyip hangi ilin olduğu yazdırılır.
+		else if (a == 9) {
+			for (int k = 0; k < ekrastgele_sayı; k++) {
+				oluşturulanil = türkiye_iileri[srastgelesayilar[0]].substr(k, 1) + türkiye_iileri[srastgelesayilar[1]].substr(k, 1) + türkiye_iileri[srastgelesayilar[2]].substr(k, 1) + türkiye_iileri[srastgelesayilar[3]].substr(k, 1) +
+					türkiye_iileri[srastgelesayilar[4]].substr(k, 1) + türkiye_iileri[srastgelesayilar[5]].substr(k, 1) + türkiye_iileri[srastgelesayilar[6]].substr(k, 1) + türkiye_iileri[srastgelesayilar[7]].substr(k, 1) + türkiye_iileri[srastgelesayilar[8]].substr(k, 1);
+				if (arama(türkiye_iileri, 81, oluşturulanil) && !bulundu) {
+					cout << " bulundu=" << "  " << oluşturulanil << endl;
+					bulundu = true;
+				}
+			}
+		}//yukarıda fonksiyona 10 sayısı girerse 10 haneli iller rastgele oluşturulur ve türkiye_iiller dizisinin içinde mi kontrol edilir içindeyse bulundu deyip hangi ilin olduğu yazdırılır.
+		else if (a == 10) {
+			for (int k = 0; k < ekrastgele_sayı; k++) {
+				oluşturulanil = türkiye_iileri[srastgelesayilar[0]].substr(k, 1) + türkiye_iileri[srastgelesayilar[1]].substr(k, 1) + türkiye_iileri[srastgelesayilar[2]].substr(k, 1) + türkiye_iileri[srastgelesayilar[3]].substr(k, 1) +
+					türkiye_iileri[srastgelesayilar[4]].substr(k, 1) + türkiye_iileri[srastgelesayilar[5]].substr(k, 1) + türkiye_iileri[srastgelesayilar[6]].substr(k, 1) + türkiye_iileri[srastgelesayilar[7]].substr(k, 1) + türkiye_iileri[srastgelesayilar[8]].substr(k, 1)
+					+ türkiye_iileri[srastgelesayilar[9]].substr(k, 1);
+				if (arama(türkiye_iileri, 81, oluşturulanil) && !bulundu) {
+					cout << " bulundu=" << "  " << oluşturulanil << endl;
+					bulundu = true;
+				}
+			}
+		}//yukarıda fonksiyona 13 sayısı girerse 13 haneli iller rastgele oluşturulur ve türkiye_iiller dizisinin içinde mi kontrol edilir içindeyse bulundu deyip hangi ilin olduğu yazdırılır.
+		else if (a == 13) {
+			for (int k = 0; k < ekrastgele_sayı; k++) {
+				oluşturulanil = türkiye_iileri[srastgelesayilar[0]].substr(k, 1) + türkiye_iileri[srastgelesayilar[1]].substr(k, 1) + türkiye_iileri[srastgelesayilar[2]].substr(k, 1) + türkiye_iileri[srastgelesayilar[3]].substr(k, 1) +
+					türkiye_iileri[srastgelesayilar[4]].substr(k, 1) + türkiye_iileri[srastgelesayilar[5]].substr(k, 1) + türkiye_iileri[srastgelesayilar[6]].substr(k, 1) + türkiye_iileri[srastgelesayilar[7]].substr(k, 1) + türkiye_iileri[srastgelesayilar[8]].substr(k, 1)
+					+ türkiye_iileri[srastgelesayilar[9]].substr(k, 1) + türkiye_iileri[srastgelesayilar[10]].substr(k, 1) + türkiye_iileri[srastgelesayilar[11]].substr(k, 1) + türkiye_iileri[srastgelesayilar[12]].substr(k, 1);
+				if (arama(türkiye_iileri, 81, oluşturulanil) && !bulundu) {
+					cout << " bulundu=" << "  " << oluşturulanil << endl;
+					bulundu = true;
+				}
+			}
+
+		}//yukarıda fonksiyona 14 sayısı girerse 14 haneli iller rastgele oluşturulur ve türkiye_iiller dizisinin içinde mi kontrol edilir içindeyse bulundu deyip hangi ilin olduğu yazdırılır.
+		else {
+			for (int k = 0; k < ekrastgele_sayı; k++) {
+				oluşturulanil = türkiye_iileri[srastgelesayilar[0]].substr(k, 1) + türkiye_iileri[srastgelesayilar[1]].substr(k, 1) + türkiye_iileri[srastgelesayilar[2]].substr(k, 1) + türkiye_iileri[srastgelesayilar[3]].substr(k, 1) +
+					türkiye_iileri[srastgelesayilar[4]].substr(k, 1) + türkiye_iileri[srastgelesayilar[5]].substr(k, 1) + türkiye_iileri[srastgelesayilar[6]].substr(k, 1) + türkiye_iileri[srastgelesayilar[7]].substr(k, 1) + türkiye_iileri[srastgelesayilar[8]].substr(k, 1)
+					+ türkiye_iileri[srastgelesayilar[9]].substr(k, 1) + türkiye_iileri[srastgelesayilar[10]].substr(k, 1) + türkiye_iileri[srastgelesayilar[11]].substr(k, 1) + türkiye_iileri[srastgelesayilar[12]].substr(k, 1) + türkiye_iileri[srastgelesayilar[13]].substr(k, 1);
+				if (arama(türkiye_iileri, 81, oluşturulanil) && !bulundu) {
+					cout << " bulundu=" << "  " << oluşturulanil << endl;
+					bulundu = true;
+				}
+			}
+		}
+
+	}
+	if (!bulundu)//il bulunamaz ise bulunamadı yazdırır
+		cout << " bulunamadı" << endl;
+
+}
+
+
+int main()
+{
+	setlocale(LC_ALL, "Turkish");
+	//illeri diziye tanımlıyorum.
+	string türkiye_iileri[81] = { {"adana"},{"adiyaman"},{"afyonkarahisar"},{"agri"},{"amasya"},{"ankara"},{"antalya"},{"artvin"},{"aydin"},{"balikesir"},{"bilecik"},{"bingol"},{"bitlis"},
+		{"bolu"},{"burdur"},{"bursa"},{"canakkale"},{"cankırı"},{"corum"},{"denizli"},{"diyarbakir"},{"edirne"},{"elazıg"},{"erzincan"},{"erzurum"},{"eskisehir"},{"gaziantep"},{"giresun"},{"gümüshane"},
+		{"hakkari"},{"hatay"},{"isparta"},{"mersin"},{"istanbul"},{"izmir"},{"kars"},{"kastomonu"},{"kayseri"},{"kırklareli"},{"kırsehir"},{"kocaeli"},{"konya"},{"kutahya"},
+		{"malatya"},{"manisa"},{"kahramanmaras"},{"mardin"},{"mugla"},{"mus"},{"nevsehir"},{"nigde"},{"ordu"},{"rize"},{"sakarya"},{"samsun"},{"siirt"},{"sinop"},{"sivas"},{"tekirdag"},
+		{"tokat"},{"trabzon"},{"tunceli"},{"sanlıurfa"},{"usak"},{"van"},{"yozgat"},{"zonguldak"},{"aksaray"},{"bayburt"},{"karaman"},{"kırıkkale"},{"batman"},{"sırnak"},{"bartın"},{"ardahan"},{"igdır"},
+		{"yalova"},{"karabük"},{"kilis"},{"osmaniye"},{"duzce"} };
+
+	// en küçük karakterli ilin indis sayısını hesaplamaya yarıyor.
+	string en_küçük_indisli_il;
+	int en_küçük_indis_sayısı = 0;
+	en_küçük_indis_sayısı = türkiye_iileri[0].size();
+	//burada en küçük indisli  sayısını bulmak için size fonksiyonu yardımıyla bir for döngüsü kurdum. Bu for döngüsü türkiye_iller[] dizisinin içinde gezerek en küçük indisli elemanı bulmamıza yardım edecek daha sonrada hangi il olduğunu bulacak.
+	for (int i = 0; i < 81; i++) {
+		if (en_küçük_indis_sayısı > türkiye_iileri[i].size()) {
+			en_küçük_indis_sayısı = türkiye_iileri[i].size();
+			en_küçük_indisli_il = türkiye_iileri[i];
+		}
+		else
+			continue;
+	}//burada yukarıda bulduğum sonuçları yazdırdım.
+	cout << "en az karakterli il:" << en_küçük_indisli_il << "   ";
+	cout << "en az karekterli ilin karekter sayısı:" << en_küçük_indis_sayısı << endl;
+
+
+	//en büyük karakterli ilin indis sayısını hesaplama.
+	int en_büyük_indis_sayısı = 0;
+	string en_büyük_indisli_il;
+	en_büyük_indis_sayısı = türkiye_iileri[0].size();
+	//burada en büyük indisli  sayısını bulmak için size fonksiyonu yardımıyla bir for döngüsü kurdum. Bu for döngüsü türkiye_iller[] dizisinin içinde gezerek en büyük indisli elemanı bulmamıza yardım edecek daha sonrada hangi il olduğunu bulacak.
+	for (int i = 0; i < 81; i++) {
+
+		if (en_büyük_indis_sayısı < türkiye_iileri[i].size()) {
+			en_büyük_indis_sayısı = türkiye_iileri[i].size();
+			en_büyük_indisli_il = türkiye_iileri[i];
+		}
+		else
+			continue;
+	}//burada yukarıda bulduğum sonuçları bastırdım
+	cout << "en büyük indisli il:" << en_büyük_indisli_il << "   ";
+	cout << "en büyük karekterli ilin karekter sayısı:" << en_büyük_indis_sayısı << endl;
+
+
+	//burada void il_oluşrma fonksiyonu çağırdım ve yazdırma işlemlerini yaptım.
+	cout << "3 harfli il:" << endl;
+	il_oluşrma(3, türkiye_iileri);
+	cout << "4 harfli il:" << endl;
+	il_oluşrma(4, türkiye_iileri);
+	cout << "5 harfli il:" << endl;
+	il_oluşrma(5, türkiye_iileri);
+	cout << "6 harfli il:" << endl;
+	il_oluşrma(6, türkiye_iileri);
+	cout << "7 harfli il:" << endl;
+	il_oluşrma(7, türkiye_iileri);
+	cout << "8 harfli il:" << endl;
+	il_oluşrma(8, türkiye_iileri);
+	cout << "9 harfli il:" << endl;
+	il_oluşrma(9, türkiye_iileri);
+	cout << "10 harfli il:" << endl;
+	il_oluşrma(10, türkiye_iileri);
+	cout << "13 harfli il:" << endl;
+	il_oluşrma(13, türkiye_iileri);
+	cout << "14 harfli il:" << endl;
+	il_oluşrma(14, türkiye_iileri);
+}
---
+++ It's about class and array.cpp
@@ -0,0 +1,184 @@
+﻿
+/****************************************************************************
+**	SAKARYA UNIVERSITY
+**	BILGISAYAR MUHENDISLIGI BOLUMU
+**	PROGRAMLAMAYA GIRIS
+**
+**	ÖĞRENCİ ADI......:MERT UMUL
+**	ÖĞRENCİ NUMARASI.:2
+**	DERS GRUBU…………………:C
+****************************************************************************/
+#include<iostream>
+#include<Windows.h>
+#include<time.h>
+using namespace std;
+/*
+windowstan tanimli renk degerleri
+LIGHTBLUE = 9
+LIGHTGREEN = 10
+LIGHTCYAN = 11
+LIGHTRED = 12
+LIGHTMAGENTA = 13
+YELLOW = 14
+BRİGHTWHİTE=15
+*/
+
+
+
+//Karakter sinifi tanimliyorum ve bu karakter sinifi harfi ve rengi tutuyor.
+class Karakter {
+public:
+	char harf;
+	int renk;
+
+};
+//Katar sınıfı tanımlıyorum bu sinifin genel amaci karakter ekleme,karakter yazdirma  ve karakter silme islemlerini yapmaktır.
+class Katar :public Karakter {
+private:
+
+	Karakter karakterler[50];//Karakter türünde sinif tanimliyorum.
+	int karakter_sayisi = 0;//karakter sayisini 0 dan baslatiyorum
+	int onceki_karakter_sayısı = 0;//onceki durumu yazdirmak icin bir tane daha karater sayisina  gerek duydugumdan bunu tanımladım
+
+public:
+	char DUZCIZGI = 205;
+	char SOLUSTKOSE = 201;
+	char SAGUSTKOSE = 187;
+	char DIKEYCIZGI = 186;
+	char ASAGIAYRAC = 203;
+	char SOLALTKOSE = 200;
+	char SAGALTKOSE = 188;
+	char YUKARIAYRAC = 202;
+
+	HANDLE R = GetStdHandle(STD_OUTPUT_HANDLE);//renkler icin gerekli tanimlama
+	//karakter ekleme islemlerinin yapildigi fonksiyon
+	void karakter_ekle() {
+		//burada gelen harf ve renk, mainden rastgele rastgele harf ve renklerdir.Bu harf ve renkleri karakterler dizisine tanimler ve karakter sayisini bir arttirir.
+		if (karakter_sayisi >= 0 && karakter_sayisi < 50 ) {
+			karakterler[karakter_sayisi].harf = harf;
+			karakterler[karakter_sayisi].renk = renk;
+			onceki_karakter_sayısı = karakter_sayisi;//burada onceki_karakter_sayisi önceki karakter_sayisi nin degerine esittir.
+			karakter_sayisi++;
+		}
+
+		else if (karakter_sayisi >= 50) {
+			SetConsoleTextAttribute(R, 4);
+
+			cout << "Ekleme Yapilamaz" << endl;
+			SetConsoleTextAttribute(R, 15);
+		}
+
+	}
+	//karakter cikarma islemlerinin yapildigi fonksiyon
+	void karakter_cikar() {
+		if (karakter_sayisi > 0) {
+			onceki_karakter_sayısı = karakter_sayisi; // burada onceki_karakter_sayisi önceki karakter_sayisi nin degerine esittir.
+			karakter_sayisi--;//karakter sayisi bir azaltilir.
+		}
+		else
+		{
+			SetConsoleTextAttribute(R, 4);//burada konsol rengini kirmiziya çeviriyorum, 4=kirmiziya rengine esittir esittir. 
+			cout << "cikarma islemi yapilamaz..." << endl;
+			SetConsoleTextAttribute(R, 15);//burada konsol rengini beyaza ceviriyorum ,15=beyaz rengine esittir.
+
+		}
+	}
+	//ekledigim veya sildigim karakterlerin sonraki durumunu gosteren fonksiyon.
+	void yazdir() {
+		cout << "sonraki durum: " << "eleman sayisi: " << karakter_sayisi << endl;
+
+		//buradaki for fonksiyonlarının amaci dikdörtgen sekillerini cikartmaktir bu yüzden bu for döngülerini kullandim
+		//dikdörtgenin üst kismini yazdirir.
+		for (int i = 0; i <= karakter_sayisi - 1 && karakter_sayisi <= 50; i++) {
+			cout << SOLUSTKOSE << DUZCIZGI << SAGUSTKOSE;
+		}
+		cout << endl;//bunun kullanmamın nedeni bir alt satira geçmek dikdörtgen yapabilmemiz icin öbür kisimlarinda alt alta olmasina ihtiyacimiz var.
+		//dikdörtgenin orta kismini yazdirir.
+		for (int i = 0; i <= karakter_sayisi - 1 && karakter_sayisi <= 50; i++) {
+			cout << DIKEYCIZGI;
+			SetConsoleTextAttribute(R, karakterler[i].renk);//burada rengi değistiriyorum karakter dizisindeki elemanin rengi neyle tanimliysa
+			cout << karakterler[i].harf;
+			SetConsoleTextAttribute(R, 15);//tekrardan beyaz renge geri ceviriyorum konsolu
+			cout << DIKEYCIZGI;
+		}
+
+		cout << endl;
+		//dikdörtgenin alt kismini yazdirir.
+		for (int i = 0; i <= karakter_sayisi - 1 && karakter_sayisi <= 50; i++) {
+			cout << SOLALTKOSE << DUZCIZGI << SAGALTKOSE;
+		}
+		cout << endl;
+	}
+	//ekledigim veya sildigim karakterlerin önceki durumunu gosteren fonksiyon.
+	void yazdır2() {
+		cout << "onceki durum: " << "eleman sayisi: " << onceki_karakter_sayısı << endl;
+		//dikdörtgenin üst kismini yazdirir.
+		for (int i = 0; i <= onceki_karakter_sayısı-1 && karakter_sayisi <= 50; i++) {
+			cout << SOLUSTKOSE << DUZCIZGI << SAGUSTKOSE;
+		}
+		cout << endl;
+		//dikdörtgenin orta kismini yazdirir.
+		for (int i = 0; i <= onceki_karakter_sayısı-1 && karakter_sayisi <= 50; i++) {
+			//if kullanmamin nedeni ilk dikdörtgen elemaninin icinin bos olamsini istememizdir.
+			if (onceki_karakter_sayısı == 0) {
+				cout << DIKEYCIZGI;
+				cout << ' ';
+				cout << DIKEYCIZGI;
+
+			}
+			else if (onceki_karakter_sayısı > 0) {
+				cout << DIKEYCIZGI;
+				SetConsoleTextAttribute(R, karakterler[i].renk);//burada rengi değistiriyorum karakter dizisindeki elemanin rengi neyle tanimliysa
+				cout << karakterler[i].harf;
+				SetConsoleTextAttribute(R, 15);//tekrardan beyaz renge geri ceviriyorum konsolu
+				cout << DIKEYCIZGI;
+			}
+		}
+		cout << endl;
+		//dikdörtgenin alt kismini yazdirir.
+		for (int i = 0; i <= onceki_karakter_sayısı-1 && karakter_sayisi <= 50; i++) {
+			cout << SOLALTKOSE << DUZCIZGI << SAGALTKOSE;
+		}
+		cout << endl;
+	}
+};
+
+int main() {
+	int secim;
+	char harfler[26] = { 'A','B','C','D','E','F','G','H','J','I','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z' };//harfleri tanimliyorum
+	Katar k1;
+	do {
+		cout << "\nIslem seciniz" << endl;
+		cout << "1.Karakter Ekle" << endl;
+		cout << "2.Karakter Cikar" << endl;
+		cout << "3.Programdan Cikis" << endl;
+		cin >> secim;
+		if (secim == 1) {
+			srand(time(NULL));
+			int harfc = rand() % 26;//rastgele 0 ila 26 arasında deger alir.
+			k1.harf = harfler[harfc];//rastgele aldığımız harf c değerini harf karsılıgini alıp harfe ekletiyorum
+			int renkc = 9 + (rand() % 5);//9 ila 14 arasi  rastgele harf sectiriyorum.
+			k1.renk = renkc; //rastgele aldığımız renkc değerini renk olarak sinifa gönderiyorum
+			//ekletme fonkisiyonlarini cagiriyorum.
+			k1.karakter_ekle();
+			k1.yazdır2();
+			k1.yazdir();
+		}
+		else if (secim == 2) {
+			//cikartma fonkisiyonlarini cagiriyorum.
+			k1.karakter_cikar();
+			k1.yazdır2();
+			k1.yazdir();
+
+		}//3 ü secerse program sonlanir.
+		else if (secim == 3) {
+			break;
+		}
+		else {
+			cout << "Lutfen dogru islemi giriniz." << endl;
+
+		}
+	} while (TRUE);
+
+}
+
